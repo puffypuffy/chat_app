66bd837ab3af057ec33d408130f975d88012d178
cz048590 <cheng.zhou@cerner.com>
2018-08-20 13:09:49 -0500

    redux basic

--- src/index.js
+++ src/index.js
@@ -1,8 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {createStore} from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+//Reducer
+function counter (state = 0, action) {
+  switch (action.type) {
+    case 'addOne':
+      return state + 1
+    case 'minusOne':
+      return state - 1
+    default:
+      return 10
+  }
+}
+
+
+//1. Create Store through Reducer
+
+const store = createStore(counter)
+
+const init = store.getState()
+console.log(`Initial Number: ${init}`)
+
+
+//3. Listern state change through subscribe(), has to sit before 2. Dispacth action
+function listener() {
+  const current = store.getState()
+  console.log(`Current Number: ${current}`)
+}
+store.subscribe(listener)
+
+
+//2. Dispatch action, to change state
+
+store.dispatch({type : 'addOne'})
+store.dispatch({ type: 'addOne' })
+store.dispatch({ type: 'addOne' })
+store.dispatch({ type: 'minusOne' })
+
+
